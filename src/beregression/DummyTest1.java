1a19d6c5b9479d705aeb5e5a781745f8d65533b8
aarnikj <aarnikj.tomar@gmail.com>
2024-04-10 15:47:59 -0400

    recent changes

---
+++ src/beregression/DummyTest1.java
@@ -0,0 +1,10 @@
+package beregression;
+
+public class DummyTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/beregression/DummyTest2.java
@@ -0,0 +1,10 @@
+package beregression;
+
+public class DummyTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
